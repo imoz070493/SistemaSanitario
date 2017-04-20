d243a456e2bf9e3c0edbb68b950271005373b2b4
imoz070493 <imoz070493@gmail.com>
2017-04-20 16:03:20 -0500

    Version1
    Creando el proyecto

---
+++ mesaAyuda/src/main/java/com/imoz/mesaAyuda/App.java
@@ -0,0 +1,13 @@
+package com.imoz.mesaAyuda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
